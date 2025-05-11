bb7e5601ef088c4cc464ab57f6c721ff9e27de60
Ruslan1493 <star93@list.ru>
2025-05-11 22:59:01 +0300

    test commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import Login from './components/Login';
 import Register from './components/Register';
 import ProtectedPage from './components/ProtectedPage';
 import Homepage from './components/Homepage';
-import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
     return (
